94712a5f0e6a141cce8d674e39f0aed92c35d246
zengrg <bijeng695@gmail.com>
2023-12-02 01:01:24 +0545

    cors

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ module.exports = {
     return [
       {
         // matching all API routes
-        source: "/api/:path*",
+        source: `${process.env.websiteLink}/api/:path*`,
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
