ad3bdaa49e41451e807e9be303861cf984867be3
zengrg <bijeng695@gmail.com>
2023-12-02 01:20:56 +0545

    link

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ module.exports = {
     return [
       {
         // matching all API routes
-        source: `${process.env.websiteLink}/api/:path*`,
+        source: "https://ecommerce-peach-iota.vercel.app/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
